26c7b706950ee84e91500d4b044a45143cb47f91
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-06-19 10:28:36 +0100

    Pre+post processing scripts

--- configs/config.py
+++ configs/config.py
@@ -21,10 +21,10 @@ from yacs.config import CfgNode as CN
 
 cfg = CN()
 
-cfg.flame_geom_path = "C:/Users/jacks/Documents/Data/3DMMs/FLAME/generic_model.pkl"
-cfg.flame_template_path = "C:/Users/jacks/Documents/Data/3DMMs/FLAME/head_template.obj"
-cfg.flame_lmk_path = "C:/Users/jacks/Documents/Data/3DMMs/FLAME/landmark_embedding.npy"
-cfg.tex_space_path = "C:/Users/jacks/Documents/Data/3DMMs/FLAME/FLAME_albedo_from_BFM.npz"
+cfg.flame_geom_path = "data/FLAME2020/generic_model.pkl"
+cfg.flame_template_path = "data/FLAME2020/head_template.obj"
+cfg.flame_lmk_path = "data/FLAME2020/landmark_embedding.npy"
+cfg.tex_space_path = "data/FLAME2020/FLAME_albedo_from_BFM.npz"
 cfg.mask_image_path = "./data/uv_dub_mask.png"
 
 cfg.num_shape_params = 300
