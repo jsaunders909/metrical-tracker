f8b817725f901dc747819993551663f58eff9d23
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-06-22 12:24:21 +0100

    HDTF Processing

--- process_HDTF.py
+++ process_HDTF.py
@@ -11,7 +11,7 @@ def main(args):
         print(f'Video {i} of {len(videos)}')
         out = os.path.join(args.output_dir, os.path.basename(video).replace('.mp4', ''))
         if not os.path.exists(out):
-            os.mkdir(out)
+            os.makedirs(out)
         cmd = f"{sys.executable} process_video.py -i {video} -o {out}"
         subprocess.call(cmd, shell=True)
         break
@@ -20,7 +20,7 @@ def main(args):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_root', type=str, required=True)
-    parser.add_argument('--output_dir', type=str, required=True)
+    parser.add_argument('-i', '--input_dir', type=str, required=True)
+    parser.add_argument('-o', '--output_dir', type=str, required=True)
     args = parser.parse_args()
     main(args)
