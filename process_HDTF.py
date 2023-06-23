d1eedcfdf400d357686e655918e1ffeae4504afa
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-06-23 12:54:26 +0100

    MICA Submodule

--- process_HDTF.py
+++ process_HDTF.py
@@ -5,6 +5,9 @@ import sys
 from multiprocessing import Pool
 
 
+def call(cmd):
+    subprocess.call(cmd, shell=True)
+
 def main(args):
 
     videos = glob(os.path.join(args.input_dir, '*.mp4'))
@@ -24,11 +27,11 @@ def main(args):
         if args.crop:
             cmd += ' --crop'
         cmds.append(cmd)
-        subprocess.call(cmd, shell=True)
+        #subprocess.call(cmd, shell=True)
 
-    #print(f'Running {len(cmds)} jobs in parallel with {args.num_workers} workers')
-    #with Pool(args.num_workers) as p:
-    #    p.map(subprocess.call, cmds)
+    print(f'Running {len(cmds)} jobs in parallel with {args.num_workers} workers')
+    with Pool(args.num_workers) as p:
+        p.map(call, cmds)
 
 
 if __name__ == '__main__':
