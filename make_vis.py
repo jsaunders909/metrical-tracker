2ba28536726f9645ec88f444206f2facc23af2ae
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-07-14 13:16:06 +0100

    Script to visualise tracking results

--- make_vis.py
+++ make_vis.py
@@ -1,19 +1,32 @@
 import os, sys, cv2
 import numpy as np
 
-n = len(os.listdir('crops'))
 
-writer = cv2.VideoWriter('vis.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30, (512 * 3, 512))
+def main(args):
+    in_dir = args.in_dir
+    out_path = args.out_path
 
-for i in range(n):
-    crop = cv2.imread(os.path.join('crops', f'{i:07d}.png'))
-    crop = cv2.resize(crop, (512, 512))
-    uv = cv2.imread(os.path.join('uv', f'{i:05d.png}'))
-    comb = ((uv.astype('float32') + crop.astype('float32')) / 2).astype('uint8')
+    n = len(os.listdir(os.path.join(in_dir, 'uv')))
 
-    frame = np.concatenate((crop, uv, comb), axis=1)
-    writer.write(frame)
+    writer = cv2.VideoWriter(args.out_path, cv2.VideoWriter_fourcc(*'mp4v'), 30, (512 * 3, 512))
 
-writer.release()
+    for i in range(n):
+        crop = cv2.imread(os.path.join(in_dir, 'crops', f'{i:07d}.png'))
+        crop = cv2.resize(crop, (512, 512))
+        uv = cv2.imread(os.path.join(in_dir, 'uv', f'{i:05d.png}'))
+        comb = ((uv.astype('float32') + crop.astype('float32')) / 2).astype('uint8')
 
+        frame = np.concatenate((crop, uv, comb), axis=1)
+        writer.write(frame)
 
+    writer.release()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_dir', type=str, required=True)
+    parser.add_argument('--out_path', type=str, required=True)
+
+    args = parser.parse_args()
+
+    main(args)
