88ac235eac1cd19ccd0b2c4298f15ad9524b7137
Jack Saunders <36443883+jsaunders909@users.noreply.github.com>
2023-07-15 15:55:17 +0100

    Alignment issues

--- process_video.py
+++ process_video.py
@@ -24,27 +24,46 @@ def main(args):
         os.mkdir(MICA_dir)
     shutil.copy(image, os.path.join(MICA_dir, '0000000.png'))
     cmd = f"{sys.executable} MICA/demo.py -i {MICA_dir} -o {args.output_dir}"
+    print('------------------------------------------------------------')
+    print('Running MICA')
+    print(cmd)
+    print('------------------------------------------------------------')
     subprocess.call(cmd, shell=True)
     shutil.copy(os.path.join(args.output_dir, '0000000', 'identity.npy'), args.output_dir)
 
     # Run the tracker
     cfg = os.path.join(args.output_dir, 'config.yaml')
     cmd = f"{sys.executable} tracker.py --cfg {cfg}"
+    print('------------------------------------------------------------')
+    print('Running tracker')
+    print(cmd)
+    print('------------------------------------------------------------')
     subprocess.call(cmd, shell=True)
 
     # Get the UVs
     checkpoint_dir = os.path.join(args.output_dir, 'config', 'checkpoint')
-    print(checkpoint_dir)
     uv_dir = os.path.join(args.output_dir, 'uv')
     cmd = f"{sys.executable} get_uv_and_mask.py --cfg {cfg} --checkpoint_dir {checkpoint_dir} --output_dir {uv_dir}"
+    print('------------------------------------------------------------')
+    print('Running UV extraction')
+    print(cmd)
+    print('------------------------------------------------------------')
     subprocess.call(cmd, shell=True)
 
     # Postprocess
     cmd = f"{sys.executable} postprocess.py --cfg {cfg} --output_dir {args.output_dir}"
+    print('------------------------------------------------------------')
+    print('Running postprocessing')
+    print(cmd)
+    print('------------------------------------------------------------')
     subprocess.call(cmd, shell=True)
 
     # Make the video for visualization
     cmd = f"{sys.executable} make_vis.py -i {args.output_dir} -o {os.path.join(args.output_dir, 'vis.mp4')}"
+    print('------------------------------------------------------------')
+    print('Running visualization')
+    print(cmd)
+    print('------------------------------------------------------------')
     subprocess.call(cmd, shell=True)
 
 
